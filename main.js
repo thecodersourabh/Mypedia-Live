b8e8053ddaea5822d5aaacce966c5b1636271dff
thecodersourabh <bmsourabhjoshi@gmail.com>
2025-01-02 22:12:06 +0530

    Auto-generated commit

--- main.js
+++ main.js
@@ -697,12 +697,12 @@ class HeaderComponent {
     this.languageFormControl = new _angular_forms__WEBPACK_IMPORTED_MODULE_6__.UntypedFormControl();
     this.cvName = '';
     this.user = null;
+  }
+  ngOnInit() {
     this.interactionService.getMessage().subscribe(message => {
       this.isAuthenticated = localStorage.getItem('isAuthenticated') === 'true';
       this.user = JSON.parse(message);
     });
-  }
-  ngOnInit() {
     this.sub = this.auth.user$.subscribe(user => {
       this.user = user;
       if (!user || !user.sub) {
